1c73098f399eaf6c84878c0872c8eb9cf9bac651
Agboola Temitope <85450650+kaiser54@users.noreply.github.com>
2023-10-23 11:27:49 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,10 @@
 <template>
   <div>
     <DynamicInput v-model="firstName" type="number" class="inputmee" />
-    <DynamicButton
-      buttonText="Click me"
-      size="micro"
-      type="link-primary"
-      icon="right"
-      :disabled="true"
-      :isLoading="true"
-      @clickButton="handleButtonClick"
-    >
-      <template v-slot:svg>
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g id="chevron-right-arrow">
-            <path
-              id="Vector"
-              d="M6.66699 3.33325L13.3337 9.99992L6.66699 16.6666"
-              stroke="#565C69"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </g>
-        </svg>
-      </template>
-    </DynamicButton>
+    <DynamicNavButton class="nav-button" buttonText="click me" url="/" />
+    <DynamicNavButton buttonText="click me please" url="" />
     <p>{{ firstName }}</p>
+    <Account name="Agboola t." />
   </div>
 </template>
 
@@ -39,7 +12,7 @@
 import { ref } from "vue";
 
 const firstName = ref("");
-const handleButtonClick = () => {
-  // Handle button click here
-};
 </script>
+
+<style scoped>
+</style>
